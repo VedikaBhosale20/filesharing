85457dbf844ce78fa2dfe00125c3a3386f78bc9d
Atharva Wagh <atharva.wagh@imfs.co.in>
2024-11-29 01:58:54 +0530

    debugging socket.io

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const http = require('http');
 const { Server } = require('socket.io');
 
 const app = express();
-const server = http.createServer(app); // Attach the Express app to the HTTP server
+const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: '*',
